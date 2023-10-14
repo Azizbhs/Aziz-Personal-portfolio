908f0fa5cb6a2e6c109091c1e6d775a9e07f97d2
Aziz <bhsaziz408@gmail.com>
2023-10-14 18:17:31 +0200

    Added scrolling

--- script.js
+++ script.js
@@ -1,3 +1,38 @@
+//scrolling
+function scrollToElement(elementSelector, instance = 0){
+    const elements = document.querySelectorAll(elementSelector);
+    if(elements.length > instance){
+        elements[instance].scrollIntoView({behavior: 'smooth'});
+    }
+}
+const link1 = document.getElementById("link1");
+const link2 = document.getElementById("link2");
+const link3 = document.getElementById("link3");
+const link4 = document.getElementById("link4");
+const link5 = document.getElementById("link5");
+
+function scrollToProjects() {
+    scrollToElement('.section-title');
+}
+
+link1.addEventListener('click', ()=>{
+    scrollToElement('.container');
+});
+
+link2.addEventListener('click', ()=>{
+    scrollToElement('.skills');
+});
+
+link4.addEventListener('click', ()=>{
+    scrollToElement('.contact');
+});
+
+link3.addEventListener('click',scrollToProjects);
+link5.addEventListener('click',scrollToProjects);
+
+
+
+
 //Emailjs
 const contactForm = document.getElementById('contact-form'),
    contactEmail = document.getElementById('contact-email')
